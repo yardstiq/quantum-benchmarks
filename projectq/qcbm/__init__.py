ac321ee72a66fb02d90948a587cd38eb64b7c168
Roger-luo <hiroger@qq.com>
2019-10-14 16:04:31 -0400

    rm old implementation

--- projectq/qcbm/__init__.py
+++
@@ -1,7 +0,0 @@
-from .qcbm import QCBM
-from .blocks import get_demo_circuit
-from .contexts import ProjectQContext, ScipyContext
-from .mmd import RBFMMD2
-from .train import train
-
-from . import utils, testsuit, qclibs, structure, blocks, dataset
