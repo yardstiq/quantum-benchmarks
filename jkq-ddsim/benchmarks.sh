fc84418c92fd9b6d1fe3110930fdde6e0fd8b38a
Stefan Hillmich <stefan.hillmich@jku.at>
2020-04-06 13:12:57 +0200

    Restablished redirection of logs

--- jkq-ddsim/benchmarks.sh
+++ jkq-ddsim/benchmarks.sh
@@ -3,15 +3,13 @@
 FILE_PATH=$(readlink -f "$0")
 BASE_PATH=$(dirname "$FILE_PATH")
 
-BENCHMARK_LOG_PATH="$1"
-BENCHMARK_DATA_PATH="$2"
+BENCHMARK_DATA_PATH="$1"
 
 cd "$BASE_PATH"
 
 # env vars do not work https://github.com/google/benchmark/issues/913
 #export BENCHMARK_OUT_FORMAT='csv'
 #export BENCHMARK_OUT="$BENCHMARK_DATA_PATH/jku-ddsim.csv"
-
 
 # to restrict the benchmarks to the single gate ones, use --benchmark_filter=BM_sim_[XHTC]
 build/apps/ddsim_benchmark --benchmark_filter=BM_sim_ --benchmark_out_format=json --benchmark_out="$BENCHMARK_DATA_PATH/jkq-ddsim.json" 
