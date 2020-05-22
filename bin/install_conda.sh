0a22c5eee2605e2f3989bdf74d615eda96ae9d64
Roger-luo <rogerluo.rl18@gmail.com>
2020-05-22 01:59:52 -0400

    choose yes

--- bin/install_conda.sh
+++ bin/install_conda.sh
@@ -22,5 +22,5 @@ mkdir -p $CONDA_PATH
 curl -L -o $INSTALLER $URL
 chmod 755 $INSTALLER
 $INSTALLER -b -f -p $CONDA_PATH
-$CONDA update -n base -c defaults conda
-$CONDA init bash
+$CONDA update -n base -c defaults conda -y
+$CONDA init bash -y
