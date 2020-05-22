e8d4fded1b18661b3ee5ce269051fee78b4d001f
Roger-luo <rogerluo.rl18@gmail.com>
2020-05-22 01:05:27 -0400

    change to yes

--- bin/install_julia.sh
+++ bin/install_julia.sh
@@ -3,4 +3,4 @@ BINDIR="$(dirname $0)"
 . $BINDIR/utils/constants.sh
 . $BINDIR/utils/colors.sh
 
-echo NY | JULIA_DOWNLOAD=$JULIA_PATH JULIA_INSTALL=$JULIA_PATH REPLY=Y bash -ci "$(curl -fsSL https://raw.githubusercontent.com/abelsiqueira/jill/master/jill.sh)"
+echo YY | JULIA_DOWNLOAD=$JULIA_PATH JULIA_INSTALL=$JULIA_PATH REPLY=Y bash -ci "$(curl -fsSL https://raw.githubusercontent.com/abelsiqueira/jill/master/jill.sh)"
