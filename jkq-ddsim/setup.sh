060ff70d5ff8060db61eaf6123d2b6ffe5bd0196
Roger-luo <rogerluo.rl18@gmail.com>
2020-05-29 23:15:02 +0000

    fix build

--- jkq-ddsim/setup.sh
+++ jkq-ddsim/setup.sh
@@ -5,14 +5,13 @@ ROOT_PATH=`dirname $FILE_PATH`
 BINDIR="$ROOT_PATH/bin"
 . $BINDIR/utils/constants.sh
 
-# cd "$BASE_PATH"
+cd "$FILE_PATH"
 
 if [ ! -d "ddsim" ]; then
     git clone --branch "v1.0.1a" --depth 1  https://github.com/iic-jku/ddsim ddsim
     git -C ddsim submodule update --init --recursive
 fi
 
-
-cmake -DGIT_SUBMODULE=OFF -DBENCHMARK_ENABLE_LTO=true -DCMAKE_BUILD_TYPE=Release -S ddsim -B ddsim/build
+cmake -DGIT_SUBMODULE=OFF -DBENCHMARK_ENABLE_LTO=true -DCMAKE_BUILD_TYPE=Release -S ddsim -B ddsim
 cmake --build build --config Release --target ddsim_benchmark
 
