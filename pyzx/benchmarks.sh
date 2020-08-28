100cc7a4816b2b125ff627fc1fec4fe6cad1b05a
Chen Zhao <zhaochen17@mails.ucas.ac.cn>
2020-08-28 17:09:47 +0800

    add benchmarks for pyzx

---
+++ pyzx/benchmarks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+FILE_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+ROOT_PATH=`dirname $FILE_PATH`
+BINDIR="$ROOT_PATH/bin"
+. $BINDIR/utils/constants.sh
+
+$ACTIVATE ./env
+pytest_benchmark
