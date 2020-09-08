07bdfce3bbd7da693cff612e2969719122dcf362
Roger-luo <rogerluo.rl18@gmail.com>
2020-09-08 01:12:34 -0400

    rm compiler benchmarks

--- zxcalculus/benchmarks.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-FILE_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-ROOT_PATH=`dirname $FILE_PATH`
-BINDIR="$ROOT_PATH/bin"
-. $BINDIR/utils/constants.sh
-
-$JULIA --project benchmarks.jl
--- zxcalculus/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-FILE_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-ROOT_PATH=`dirname $FILE_PATH`
-BINDIR="$ROOT_PATH/bin"
-. $BINDIR/utils/constants.sh
-
-$JULIA --color=yes -e "using Pkg;Pkg.activate(@__DIR__);Pkg.instantiate(verbose=true);Pkg.precompile()"
