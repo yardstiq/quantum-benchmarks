aa1f7f6a7e61779f7ca33b6a5ca64b65342be698
Chen Zhao <50411747+ChenZhao44@users.noreply.github.com>
2020-09-24 17:47:11 +0800

    Update benchmarks for PyZX and ZXCalculus (#23)
    * update deps
    
    * save as JSON
    
    * use pytest
    
    * add results

--- pyzx/benchmarks.py
+++ pyzx/benchmarks.py
@@ -1,22 +1,18 @@
 import random, math, os, time
 import pyzx as zx
+import pytest
 
-def run_benchmark():
-    dir = 'circuits/'
-    filenames = os.listdir(dir)
-    bms = dict()
-    for circ_name in filenames:
-        circ_path = dir + circ_name
-        c = zx.Circuit.load(circ_path).to_basic_gates()
-        g = c.to_graph()
-        t0 = time.time()
-        g = zx.simplify.teleport_reduce(g)
-        t1 = time.time()
-        t = t1 - t0
-        tc = zx.tcount(g)
-        print(circ_name + '\t time =', t, '\t tcount =', tc)
-        bms[circ_name] = t
-    return bms
+dir = 'circuits/'
+filenames = os.listdir(dir)
+filenames.sort()
 
-print('Benchmarking PyZX...')
-bms = run_benchmark()
+def run_phase_teleportation(benchmark, circ_name):
+    circ_path = dir + circ_name
+    c = zx.Circuit.load(circ_path).to_basic_gates()
+    g = c.to_graph()
+    benchmark(zx.simplify.teleport_reduce, g)
+
+@pytest.mark.parametrize('circ_name', filenames)
+def test_PyZX(benchmark, circ_name):
+    benchmark.group = circ_name
+    run_phase_teleportation(benchmark, circ_name)
--- zxcalculus/benchmarks.jl
+++ zxcalculus/benchmarks.jl
@@ -1,27 +1,40 @@
 using YaoLang: YaoIR, is_pure_quantum
 using ZXCalculus
-using BenchmarkTools
+using BenchmarkTools, JSON
 
 function zx_load_qasm(filename::String)
     srcs = readlines("circuits/$(filename)")
     src = prod([srcs[1]; srcs[3:end]])
-    m = @__MODULE__
-    ir = YaoIR(m, src, :qasm_circ)
-    ir.pure_quantum = is_pure_quantum(ir)
-    zxd = ZXDiagram(ir)
+    # m = @__MODULE__
+    # ir = YaoIR(m, src, :qasm_circ)
+    # ir.pure_quantum = is_pure_quantum(ir)
+    # zxd = ZXDiagram(ir)
+    zxd = ZXDiagram(src, Val(:qasm))
     return zxd
 end
 function run_benchmark()
     filenames = readdir("circuits")
-    bms = Dict()
+    benchmarks = Dict()
     for circ_name in filenames
+        println("benchmarking $circ_name")
         zxd = zx_load_qasm(circ_name)
         b = @benchmark phase_teleportation($zxd)
-        println(circ_name, "\t time = ", (mean(b).time / 1e9), tcount(phase_teleportation(zxd)))
-        bms[circ_name] = b
+        tc = tcount(phase_teleportation(zxd))
+        # println(circ_name, "\ttime = ", (mean(b).time / 1e9), "(s)\tT-count = ", tc)
+        benchmarks[circ_name] = Dict()
+        benchmarks[circ_name]["times"] = Dict()
+        benchmarks[circ_name]["times"]["mean"] = mean(b).time
+        benchmarks[circ_name]["times"]["min"] = minimum(b).time
+        benchmarks[circ_name]["times"]["max"] = maximum(b).time
+        benchmarks[circ_name]["T-count"] = tc
     end
-    return bms
+    return benchmarks
 end
 
-println("Benchmarking ZXCalculus.jl...")
-bms = run_benchmark()
+benchmarks = run_benchmark()
+
+if !ispath("data")
+    mkpath("data")
+end
+
+write("data/data.json", JSON.json(benchmarks))
