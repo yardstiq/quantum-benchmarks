c055501d617910e0fe211ca5e2d691af89fe2bc2
Daniel Strano <stranoj@gmail.com>
2021-12-30 23:25:00 -0500

    Finish() before exit, not inside loop

--- qrack/benchmarks.cc
+++ qrack/benchmarks.cc
@@ -12,8 +12,8 @@ static void BM_sim_X(benchmark::State& state) {
 	QInterfacePtr qReg = CreateQuantumInterface({ QINTERFACE_HYBRID }, state.range(0), 0); 
 	for (auto _ : state){
 		qReg->X(state.range(0) - 1);
-		qReg->Finish();
 	}
+	qReg->Finish();
 	state.SetLabel("X");
 }
 // Register the function as a benchmark
@@ -23,8 +23,8 @@ static void BM_sim_H(benchmark::State& state) {
 	QInterfacePtr qReg = CreateQuantumInterface({ QINTERFACE_HYBRID }, state.range(0), 0);
 	for (auto _ : state){
 		qReg->H(state.range(0) - 1);
-		qReg->Finish();
 	}
+	qReg->Finish();
 	state.SetLabel("H");
 }
 // Register the function as a benchmark
@@ -34,8 +34,8 @@ static void BM_sim_T(benchmark::State& state) {
 	QInterfacePtr qReg = CreateQuantumInterface({ QINTERFACE_HYBRID }, state.range(0), 0);
 	for (auto _ : state){
 		qReg->T(state.range(0) - 1);
-		qReg->Finish();
 	}
+	qReg->Finish();
 	state.SetLabel("T");
 }
 // Register the function as a benchmark
@@ -45,8 +45,8 @@ static void BM_sim_CNOT(benchmark::State& state) {
 	QInterfacePtr qReg = CreateQuantumInterface({ QINTERFACE_HYBRID }, state.range(0), 0);
 	for (auto _ : state){
 		qReg->CNOT(0,1);
-		qReg->Finish();
 	}
+	qReg->Finish();
 	state.SetLabel("CNOT");
 }
 // Register the function as a benchmark
@@ -56,8 +56,8 @@ static void BM_sim_Toffoli(benchmark::State& state) {
 	QInterfacePtr qReg = CreateQuantumInterface({ QINTERFACE_HYBRID }, state.range(0), 0);
 	for (auto _ : state){
 		qReg->CCNOT(0,1,2);
-		qReg->Finish();
 	}
+	qReg->Finish();
 	state.SetLabel("Toffoli");
 }
 // Register the function as a benchmark
@@ -67,8 +67,8 @@ static void BM_sim_Rx(benchmark::State& state) {
 	QInterfacePtr qReg = CreateQuantumInterface({ QINTERFACE_HYBRID }, state.range(0), 0);
 	for (auto _ : state){
 		qReg->RX(0.5, 2);
-		qReg->Finish();
 	}
+	qReg->Finish();
 	state.SetLabel("Rx");
 }
 // Register the function as a benchmark
@@ -78,8 +78,8 @@ static void BM_sim_Ry(benchmark::State& state) {
 	QInterfacePtr qReg = CreateQuantumInterface({ QINTERFACE_HYBRID }, state.range(0), 0);
 	for (auto _ : state){
 		qReg->RY(0.5, 2);
-		qReg->Finish();
 	}
+	qReg->Finish();
 	state.SetLabel("Ry");
 }
 // Register the function as a benchmark
