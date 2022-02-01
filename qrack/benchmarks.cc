40b73c0b20a1ed67d97d585b0c9e961487d58e38
Daniel Strano <stranoj@gmail.com>
2022-02-01 10:40:52 -0500

    isKet bool

--- qrack/benchmarks.cc
+++ qrack/benchmarks.cc
@@ -6,10 +6,16 @@
 
 using namespace Qrack; 
 
+// This suite is meant to test "ket" simulation performance specifically,
+// but Qrack can sometimes do much better than this with its default optimal layer stack!
+// Toggle the bool below to try both methods.
+const bool isKet = true;
+const auto SimType = isKet ? QINTERFACE_HYBRID : QINTERFACE_OPTIMAL_MULTI;
+
 auto min_estimator = [](const std::vector<double>& v) -> double {return *(std::min_element(std::begin(v), std::end(v)));};
 
 static void BM_sim_X(benchmark::State& state) {
-	QInterfacePtr qReg = CreateQuantumInterface({ QINTERFACE_HYBRID }, state.range(0), 0); 
+	QInterfacePtr qReg = CreateQuantumInterface({ SimType }, state.range(0), 0);
 	for (auto _ : state){
 		qReg->X(state.range(0) - 1);
 	}
@@ -20,7 +26,7 @@ static void BM_sim_X(benchmark::State& state) {
 BENCHMARK(BM_sim_X)->DenseRange(4,25); //->ComputeStatistics("min", min_estimator);
 
 static void BM_sim_H(benchmark::State& state) {
-	QInterfacePtr qReg = CreateQuantumInterface({ QINTERFACE_HYBRID }, state.range(0), 0);
+	QInterfacePtr qReg = CreateQuantumInterface({ SimType }, state.range(0), 0);
 	for (auto _ : state){
 		qReg->H(state.range(0) - 1);
 	}
@@ -31,7 +37,7 @@ static void BM_sim_H(benchmark::State& state) {
 BENCHMARK(BM_sim_H)->DenseRange(4,25); //->ComputeStatistics("min", min_estimator);
 
 static void BM_sim_T(benchmark::State& state) {
-	QInterfacePtr qReg = CreateQuantumInterface({ QINTERFACE_HYBRID }, state.range(0), 0);
+	QInterfacePtr qReg = CreateQuantumInterface({ SimType }, state.range(0), 0);
 	for (auto _ : state){
 		qReg->T(state.range(0) - 1);
 	}
@@ -42,7 +48,7 @@ static void BM_sim_T(benchmark::State& state) {
 BENCHMARK(BM_sim_T)->DenseRange(4,25); //->ComputeStatistics("min", min_estimator);
 
 static void BM_sim_CNOT(benchmark::State& state) {
-	QInterfacePtr qReg = CreateQuantumInterface({ QINTERFACE_HYBRID }, state.range(0), 0);
+	QInterfacePtr qReg = CreateQuantumInterface({ SimType }, state.range(0), 0);
 	for (auto _ : state){
 		qReg->CNOT(0,1);
 	}
@@ -53,7 +59,7 @@ static void BM_sim_CNOT(benchmark::State& state) {
 BENCHMARK(BM_sim_CNOT)->DenseRange(4,25); //->ComputeStatistics("min", min_estimator);
 
 static void BM_sim_Toffoli(benchmark::State& state) {
-	QInterfacePtr qReg = CreateQuantumInterface({ QINTERFACE_HYBRID }, state.range(0), 0);
+	QInterfacePtr qReg = CreateQuantumInterface({ SimType }, state.range(0), 0);
 	for (auto _ : state){
 		qReg->CCNOT(0,1,2);
 	}
@@ -64,7 +70,7 @@ static void BM_sim_Toffoli(benchmark::State& state) {
 BENCHMARK(BM_sim_Toffoli)->DenseRange(4,25); //->ComputeStatistics("min", min_estimator);
 
 static void BM_sim_Rx(benchmark::State& state) {
-	QInterfacePtr qReg = CreateQuantumInterface({ QINTERFACE_HYBRID }, state.range(0), 0);
+	QInterfacePtr qReg = CreateQuantumInterface({ SimType }, state.range(0), 0);
 	for (auto _ : state){
 		qReg->RX(0.5, 2);
 	}
@@ -75,7 +81,7 @@ static void BM_sim_Rx(benchmark::State& state) {
 BENCHMARK(BM_sim_Rx)->DenseRange(4,25); //->ComputeStatistics("min", min_estimator);
 
 static void BM_sim_Ry(benchmark::State& state) {
-	QInterfacePtr qReg = CreateQuantumInterface({ QINTERFACE_HYBRID }, state.range(0), 0);
+	QInterfacePtr qReg = CreateQuantumInterface({ SimType }, state.range(0), 0);
 	for (auto _ : state){
 		qReg->RY(0.5, 2);
 	}
@@ -87,7 +93,7 @@ static void BM_sim_Ry(benchmark::State& state) {
 BENCHMARK(BM_sim_Ry)->DenseRange(4,25); //->ComputeStatistics("min", min_estimator);
 
 static void BM_sim_QCBM(benchmark::State& state) {
-        QInterfacePtr qReg = CreateQuantumInterface({ QINTERFACE_HYBRID }, state.range(0), 0);
+        QInterfacePtr qReg = CreateQuantumInterface({ SimType }, state.range(0), 0);
         for (auto _: state){
 
                // First rotation
